25dce20527a1a4adeb44689c40f773c16dfb9165
Darren Murphy <darren@victoriajd.com>
2023-09-03 23:51:17 +0800

    Update function.js

--- function.js
+++ function.js
@@ -1,14 +1,14 @@
 window.function = function (input, key) {
+  const CryptoJS = require('crypto-js');
   const message = input.value;
   const secretKey = key.value;
-  import('https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.9-1/crypto-js.js')
-    .then((CryptoJS) => {
+  try {
     const hmacSha1 = CryptoJS.HmacSHA1(message, key);
     const hexEncodedHash = hmacSha1.toString(CryptoJS.enc.Hex);
     return hexEncodedHash;
-  })
-  .catch((error) => {
+  }
+  catch(error) {
     return "Error loading CryptoJS:";
-  });
+  }
 }
 

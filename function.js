5f325ceb591fee143b62b7ce9b512707b347027d
Darren Murphy <darren@victoriajd.com>
2023-09-03 23:18:30 +0800

    Update function.js

--- function.js
+++ function.js
@@ -1,12 +1,14 @@
 window.function = function (input, key) {
   const message = input.value;
   const secretKey = key.value;
-  try {
-    const hmac = CryptoJS.HmacSHA1(message, secretKey);
-    const hashHex = hmac.toString(CryptoJS.enc.Hex);
-  }
-  catch (err) {
-    return "error";
-  }
-  return hashHex;
+  import('https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.9-1/crypto-js.js')
+    .then((CryptoJS) => {
+    const hmacSha1 = CryptoJS.HmacSHA1(message, key);
+    const hexEncodedHash = hmacSha1.toString(CryptoJS.enc.Hex);
+    return hexEncodedHash;
+  })
+  .catch((error) => {
+    return "Error loading CryptoJS:";
+  });
 }
+

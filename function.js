3b3938a627d3227b5f3cf1e7e208b8ff4a5cc156
Darren Murphy <darren@victoriajd.com>
2023-09-03 22:54:34 +0800

    Update function.js

--- function.js
+++ function.js
@@ -2,4 +2,4 @@ const message = 'hello';
 const secretKey = 'secret';
 const hmac = CryptoJS.HmacSHA1(message, secretKey);
 const hashHex = hmac.toString(CryptoJS.enc.Hex);
-console.log(hashHex)
+return hashHex;
